d7eb135369cc1238091556e181815dab32ddbe2c
Matthew Dowling <92895537+MattD8957@users.noreply.github.com>
2024-10-15 13:09:44 -0400

    Update sum.c
    Formatting

--- sum.c
+++ sum.c
@@ -3,27 +3,28 @@
 int main () //start of main function
 {
 	//list of variables
-int value=0;
-int sum = 0;
-//prompt user for integer
+	int value=0;
+	int sum = 0;
+	
+	//prompt user for integer
 	printf("Please enter a positive integer\n");
 	scanf("%d",&value);
-//create for loop based on user's answer
-for(int i=0; i<=value;i++)
-	{
-	sum = sum + i;//adds to sum by each interval
-	if(i>0)
-	{
-		if(i<2)
+	
+	//create for loop based on user's answer
+	for(int i=0; i<=value;i++) {
+		sum = sum + i;//adds to sum by each interval
+		if(i>0) 
 		{
-			printf("%d",i);
-		}
-		else
-		{
-			printf("+%d",i);
+			if(i<2) 
+			{
+				printf("%d",I);
+			}
+			else 
+			{
+				printf("+%d",I);
+			}
 		}
 	}
-	}
-printf("=%d\n",sum);//tell user results
-return 0;
+	printf("=%d\n",sum);//tell user results
+	return 0;
 }
